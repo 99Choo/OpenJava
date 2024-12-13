c7c33ecf4acf9f1ec44fc16f607f0632920ae513
99Choo <chush4649@gmail.com>
2024-12-13 13:38:07 +0900

    import_java_awt

--- Jtable/src/mysql/jdbc.java
+++ Jtable/src/mysql/jdbc.java
@@ -1,6 +1,7 @@
 package mysql;
 import javax.swing.*; //GUI 관련 클래스와 컴포넌트 제공
 import javax.swing.table.DefaultTableModel; // 테이블의 데이터를 관리
+import java.awt.*; //GUI 만들기 위한 도구와 클래스들을 제공. 창,버튼,텍스트 필드,레이아웃 매니저 등
 public class jdbc {
 	
 
